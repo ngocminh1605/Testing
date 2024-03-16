40f3b7d595945b177340987ddfc27b5f56869b2c
ngocminh1605 <100121817+ngocminh1605@users.noreply.github.com>
2024-03-16 23:33:29 +0700

    Update Bai1.java

--- Bai1.java
+++ Bai1.java
@@ -1,6 +1,9 @@
 public class Bai1 {
     public static int calculateProductPrice(char productType, int totalQuantity) {
-        int price = 0;
+        int price = -1;
+        if ( totalQuantity < 1 || totalQuantity > 999) {
+            return price;
+        }
         if (productType == 'A') {
             if (totalQuantity >= 100) {
                 price = 100000;
@@ -19,8 +22,13 @@ public class Bai1 {
 
     public static void main(String[] args) {
         // Kiểm thử với các trường hợp biên
-        char[] productTypes = {'A', 'A', 'A', 'A', 'B', 'B', 'B', 'B'};
-        int[] totalQuantities = {1, 99, 100, 999, 1, 99, 100, 999};
+        char[] productTypes ={'A', 'A', 'A', 'A', 'A', 'A', 'A', ,'B', 'B', 'B','B', 'B', 'B', 'B'};
+        int[] totalQuantities = {1, 2, 0, 500,999,1000,998,1, 2, 0, 500,999,1000,998};
+
+        // Kiểm thử với bảng giá trị
+        //char[] productTypes ={'A', 'A', 'A', 'A','B', 'B', 'B','B'};
+        //int[] totalQuantities = {0,2,500,2000,0,2,500,2000};
+
 
         for (int i = 0; i < productTypes.length; i++) {
             int price = calculateProductPrice(productTypes[i], totalQuantities[i]);
